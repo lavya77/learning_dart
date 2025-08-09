c4757d885f1c9e121596a2e426248dff0e014d59
Lavya <lavya203@gmail.com>
2025-08-09 22:42:49 +0530

    datatypes in dart

---
+++ startingwithdart.dart
@@ -0,0 +1,21 @@
+void main(){
+
+//Strings
+String firstName = "John";
+print("String: $firstName");
+//Integers
+int num = 23;
+print("Integer: $num");
+
+//Doubles
+double num2 = 12.25;
+print("Double: $num2");
+//booleans
+bool mybool =false;
+print("Boolean: $mybool");
+
+//Dynamic
+
+dynamic name = "Lavya";
+print("Dynamic : $name");
+}
