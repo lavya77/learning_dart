865b7e5dafd84712742a145749148368b9c51346
Lavya <lavya203@gmail.com>
2025-08-10 00:41:26 +0530

    If Else ELsse if in Dart

---
+++ logic.dart
@@ -0,0 +1,14 @@
+void main(){
+  //logic - if , if/elsse , if else if
+  var num= 51;
+  if(num == 5){
+    print("The number is 5");
+  }
+  else if(num == 3){
+    print("The number is 3!");
+
+  }
+  else{
+    print("The number is niether 5 nor 3!, it is $num");
+  }
+}
